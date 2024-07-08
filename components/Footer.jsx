c92258358fded5d19e752bfd77eefb07e854840d
Furkan Doğu <furkandogu2018@gmail.com>
2024-07-08 20:33:39 +0300

    footer bitti

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Stack, Typography } from "@mui/material"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Stack bgcolor={"#0C0C0C"} height={"10vh"} justifyContent={"center"} alignItems={"center"}>
+      <Typography color={"white"}>
+        Copyright &copy; 2024 by Furkan Doğu
+      </Typography>
+    </Stack>
   )
 }
 
