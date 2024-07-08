61116146c1f06a7151e452f7f42a2d7ed11edfe5
Furkan Doğu <furkandogu2018@gmail.com>
2024-07-08 20:28:53 +0300

    navbar tamamlandı. redux yapısı kurulunca düzenleme yapılacak

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,46 +1,29 @@
-"use client"
+"use client";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { useState } from 'react';
-import LOGO from "@/public/blog-app.jpg"
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-
-// const pages = ['Products', 'Pricing', 'Blog'];
-const pages = [
-  {
-    id: 1,
-    title: "Dashboard",
-    link: "/"
-  },
-  {
-    id: 1,
-    title: "New Blog",
-    link: "/new-blog"
-  },
-  {
-    id: 1,
-    title: "About",
-    link: "/about"
-  },
-]
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+import LOGO from "@/public/blog-app.jpg";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { pages, settings } from "@/helpers/navbarLinks";
 
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
-  const router = useRouter()
+  const router = useRouter();
+  const user = false;
+  const image = ""
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -59,13 +42,13 @@ function Navbar() {
 
   return (
     <AppBar position="static">
-      <Container maxWidth="xl" sx={{backgroundColor: "#0C0C0C"}}>
+      <Container maxWidth="xl" sx={{ backgroundColor: "#0C0C0C" }}>
         <Toolbar disableGutters>
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
-            <Image src={LOGO} alt='LOGO' width={120} height={70} />
+          <Box sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}>
+            <Image src={LOGO} alt="LOGO" width={120} height={70} />
           </Box>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -80,42 +63,45 @@ function Navbar() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page.id} onClick={() => {
-                  handleCloseNavMenu();
-                  router.push(page.link)
-                  }}>
+                <MenuItem
+                  key={page.id}
+                  onClick={() => {
+                    handleCloseNavMenu();
+                    router.push(page.link);
+                  }}
+                >
                   <Typography textAlign="center">{page.title}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' }, flexGrow: 1 }}>
-            <Image src={LOGO} alt='LOGO' width={120} height={70} />
+          <Box sx={{ display: { xs: "flex", md: "none" }, flexGrow: 1 }}>
+            <Image src={LOGO} alt="LOGO" width={120} height={70} />
           </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page.id}
                 onClick={() => {
                   handleCloseNavMenu();
-                  router.push(page.link)
+                  router.push(page.link);
                 }}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
                 {page.title}
               </Button>
@@ -125,30 +111,47 @@ function Navbar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt={user} src={image} />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+              {user ? (
+                settings.map((setting) => (
+                  <MenuItem
+                    key={setting.id}
+                    onClick={() => {
+                      handleCloseUserMenu();
+                      router.push(setting.link);
+                    }}
+                  >
+                    <Typography textAlign="center">{setting.title}</Typography>
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem
+                  onClick={() => {
+                    handleCloseUserMenu();
+                    router.push("/login");
+                  }}
+                >
+                  <Typography textAlign="center">Login</Typography>
                 </MenuItem>
-              ))}
+              )}
             </Menu>
           </Box>
         </Toolbar>
---
+++ helpers/navbarLinks.js
@@ -0,0 +1,35 @@
+export const pages = [
+    {
+        id: 1,
+        title: "Dashboard",
+        link: "/"
+    },
+    {
+        id: 2,
+        title: "New Blog",
+        link: "/new-blog"
+    },
+    {
+        id: 3,
+        title: "About",
+        link: "/about"
+    }
+]
+
+export const settings = [
+    {
+        id: 1,
+        title: "My Blogs",
+        link: "/my-blogs"
+    },
+    {
+        id: 2,
+        title: "Profile",
+        link: "/profile"
+    },
+    {
+        id: 3,
+        title: "Logout",
+        link: "/"
+    }
+  ]
