6034ca462770d0b9fe5d74c186dca5a795176696
Furkan Doğu <furkandogu2018@gmail.com>
2024-07-08 19:11:29 +0300

    navbar düzenlenecek

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/furkan-logo.jpg" sizes="any" />
+      </head>
       <body className={inter.className}>
         <CssBaseline />
         <Navbar />
